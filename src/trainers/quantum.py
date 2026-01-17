184fbc5654c03c4b07afb27eb417c8f44f8c4f60
anishfelixm <anishfelixm.cs20@rvce.edu.in>
2026-01-17 23:50:26 +0530

    feat : updates

--- src/trainers/quantum.py
+++ src/trainers/quantum.py
@@ -1,13 +1,5 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader, Subset
-import numpy as np
-
-from src.models.qml_vqc import QuantumClassifier
-
+import pennylane as qml
+from pennylane.numpy import numpy as np
 
 def train_vqc(*args):
     return 0.0
